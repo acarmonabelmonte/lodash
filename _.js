13176b71ecbfe588019ab6ef0fcbf6920ac20dc0
Adrián Carmona <acarmona.dev@gmail.com>
2020-12-13 16:18:06 +0100

    implementing inRange method and testing :ok_hand:

--- _.js
+++ _.js
@@ -4,11 +4,22 @@ const _ = {
         let lowerClampedValue = Math.max(number, lower);
         let clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
+    },
+
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        }
+
+        if (end < start) {
+            let temp = end;
+            end = start;
+            start = temp;
+        }
+        return number >= start && number < end;
     }
 };
-
-
-
 
 // Do not write or modify code below this line.
 module.exports = _;
