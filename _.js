85a5a3928b87109d040aa9b9952c7e9fd055044d
Adrián Carmona <acarmona.dev@gmail.com>
2020-12-13 15:58:32 +0100

    implementing clamp method and testing :ok_hand:

--- _.js
+++ _.js
@@ -1,4 +1,11 @@
-const _ = {};
+// Before we get started implementing our new methods, we need to create an object to contain them
+const _ = {
+    clamp(number, lower, upper) {
+        let lowerClampedValue = Math.max(number, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    }
+};
 
 
 
